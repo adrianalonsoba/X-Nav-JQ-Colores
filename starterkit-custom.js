b0597b21fd023be8fe00f0c9aee151ae71e6a93b
adrianalonsoba <adrian.alonsoba@gmail.com>
2015-02-10 19:56:26 +0100

    ejercicio 5.6.2 terminado

---
+++ starterkit-custom.js
@@ -0,0 +1,19 @@
+// Fade out text when clicking over some other...
+// ...and fade it back when clicking over some other.
+//
+jQuery(document).ready(function() {
+    $("#faq dt").click(function() {
+		$(this).fadeOut("slow");
+    });
+    $("h3").click(function() {
+		$("dt").fadeIn("slow");
+    });
+    $('#orderedlist').addClass('red');
+    $('#orderedlist li').addClass('blue');
+    $('#orderedlist li:last').hover(function(){
+    	$(this).addClass('green');
+    },function(){
+    	$(this).removeClass('green');
+    });
+
+});
